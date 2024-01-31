b50cd2b06de39a1a9abccc4c7968fff2fa24f6af
antonio <antoniorc7@gmail.com>
2024-01-31 19:18:41 +0100

    Tarea 10

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-for (let i=100; i>=0; i--) {
-    if (i % 3 == 0 || i % 2 == 0) {
-        console.log(i);
-}
+let numeroventas = parseInt(prompt("introduzca el número de ventas que quiere incluir en la lista"))
 
+let lista = [];
+
+for (let i = 0; i < numeroventas; i++) {
+    lista.push(prompt("dime un juego"))
 }
+console.log(lista)
